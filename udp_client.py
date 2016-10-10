d8fead7edde95f3fea6c2d6812337a96cbd03124
Unknown <redbeansodapop@hotmail.com>
2016-10-10 17:52:45 +0800

    Some fixes, for working on xterm.

--- udp_client.py
+++ udp_client.py
@@ -6,8 +6,7 @@ import hashlib
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-m = hashlib.md5()
-server_address = ('localhost', 5555)
+server_address = ('10.0.0.1', 5555)
 
 # Input bit per second here
 bit_per_second = 100000000
@@ -18,13 +17,14 @@ message = str(segment_number) + "," + 'This is the message.  It will be repeated
 try:
     while (segment_number <= (byte_per_second + len(message))):
         message = str(segment_number) + "," + 'This is the message.  It will be repeated,'
+        m = hashlib.md5()
         m.update(message)
         message += m.digest()[:4]
         # Send data
         print >> sys.stderr, 'sending "%s"' % message
         sent = sock.sendto(message, server_address)
         segment_number += len(message)
-        time.sleep(len(message) / byte_per_second)
+        #time.sleep(len(message) / byte_per_second)
 
 
 
--- udp_server.py
+++ udp_server.py
@@ -7,26 +7,28 @@ import time
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to the port
-server_address = ('localhost', 5555)
+server_address = ('10.0.0.1', 5555)
 print >> sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 received_data = []
-m = hashlib.md5()
 
 
 def verify_incoming_data(data):
     data_list = data.split(",", 2)
+    m = hashlib.md5()
     m.update(data_list[0] + "," + data_list[1] + ",")
     if not data_list[2] == m.digest()[:4]:
+        print data_list[2]
+        print m.digest()[:4]
         print('WARNING: #{0} segment is corrupted'.format(data_list[0]))
 
 
 def verify_received_data_order(received_data):
     segment_number = 0
-    sorted(received_data, key=lambda x: x.split(",")[0])
+    received_data = sorted(received_data, key=lambda x: int(x.split(",")[0]))
     for data in received_data:
         if segment_number != int(data.split(",")[0]):
-            print("WARNING: ERROR AT {0}".format(data))
+            print("WARNING: {0}, SUPPOSED TO BE {1}".format(data, segment_number))
         segment_number += len(data)
 
 
@@ -37,7 +39,6 @@ while True:
     received_data.append(data)
     verify_incoming_data(data)
 
-    print >> sys.stderr, 'received %s bytes from %s' % (len(data), address)
-    print >> sys.stderr, data
+    #print >> sys.stderr, 'received %s bytes from %s: %s' % (len(data), address,data)
 
 verify_received_data_order(received_data)
